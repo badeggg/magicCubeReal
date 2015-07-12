938192556bcb91da06903bd7054a2670219bd762
badeggg <zhaoxuxujc@163.com>
2015-07-12 20:25:46 +0800

    完成了最核心的魔方代码
    这次的主要工作是：1、提升“数据库”db。因为db永远不需要改变，不需要在函数内部构建，所以把db弄到了最外层。
    
    2、为了防止被“看穿”，在rotateCells()内添加了preventGoof()函数，在buildCubeFragment()函数内添加了buildMasks()函数

--- js/magicCube.js
+++ js/magicCube.js
@@ -4,65 +4,264 @@
 		rank: 3, //grids of cube
 		scale: 1 //scale visual size of the cube 
 	};
+	var database = {
+	//'数据库'存储着魔方信息
+		buildSurfaces: {
+		//"数据库"。存储着构建surfaces需要的信息
+		//用于帮助设置小方块的floor/color attribute, style.transform等
+		//查询出floor的轴线方向, 部署到各个平面的rotate值
+			front: {
+				row: 'X',
+				column: 'Y',
+				color: 'blue',
+				rotate: '',
+				translateAxis: 'Z',
+				translateSign: '+'
+			},
+			left: {
+				row: 'Z',//例子：查询row,colum可知在left面，主坐标轴是Z，副坐标轴是Y（对应常见坐标系里的 X, Y）
+				column: 'Y',
+				color: 'orange',
+				rotate: 'rotateY(-90deg)',//例子：查询rotate,translateAxis,translateSign信息可知，在left面上的cells需要rotateY(-90deg) & translateX(-(110*rank/2)px)
+				translateAxis: 'X',
+				translateSign: '-'
+			},
+			top: {
+				row: 'X',
+				column: 'Z',
+				color: 'yellow',
+				rotate: 'rotateX(90deg)',
+				translateAxis: 'Y',
+				translateSign: '-'
+			},
+			back: {
+				row: 'X',
+				column: 'Y',
+				color: 'green',
+				rotate: '',
+				translateAxis: 'Z',
+				translateSign: '-'
+			},
+			right: {
+				row: 'Z',
+				column: 'Y',
+				color: 'red',
+				rotate: 'rotateY(-90deg)',
+				translateAxis: 'X',
+				translateSign: '+'
+			},
+			bottom: {
+				row: 'X',
+				column: 'Z',
+				color: 'whitesmoke',
+				rotate: 'rotateX(90deg)',
+				translateAxis: 'Y',
+				translateSign: '+'
+			}
+		},
+		buildButtons: {
+		//"数据库"。存储着构建buttons需要的信息（控制旋转信息）
+			front: {
+				columnStart: '+X',//例,查询此条信息可知在front面的最上面的一排按钮的控制行为是：绕x轴正向旋转90度
+				columnEnd: '-X',
+				rowStart: '-Y',
+				rowEnd: '+Y'
+			},
+			left: {
+				columnStart: '+Z',
+				columnEnd: '-Z',
+				rowStart: '-Y',
+				rowEnd: '+Y'
+			},
+			top: {
+				columnStart: '+X',
+				columnEnd: '-X',
+				rowStart: '-Z',
+				rowEnd: '+Z'
+			},
+			back: {
+				columnStart: '-X',
+				columnEnd: '+X',
+				rowStart: '+Y',
+				rowEnd: '-Y'
+			},
+			right: {
+				columnStart: '-Z',
+				columnEnd: '+Z',
+				rowStart: '+Y',
+				rowEnd: '-Y'
+			},
+			bottom: {
+				columnStart: '-X',
+				columnEnd: '+X',
+				rowStart: '+Z',
+				rowEnd: '-Z'
+			}
+		},
+		updateAttributeOfCells: {
+		//“数据库”存储着更新cells的attribute(surface, floor)所需要的信息
+			'+X': {
+				'top': {
+					next: 'back',//例子：查询此条信息可知，绕X轴正向旋转90度，top surface 将变成back surface
+					axisFrom: 'Z',//标明Z轴方向上的floor将变成Y轴方向上的floor，且层数调换。如：floor 'X1Z2'变为'X1Y0'(3阶魔方)
+					axisTo: 'Y',
+					axisReverse: true
+				},
+				'back': {
+					next: 'bottom',
+					axisFrom: 'Y',
+					axisTo: 'Z',
+					axisReverse: false
+				},
+				'bottom': {
+					next: 'front',
+					axisFrom: 'Z',
+					axisTo: 'Y',
+					axisReverse: true
+				},
+				'front': {
+					next: 'top',
+					axisFrom: 'Y',
+					axisTo: 'Z',
+					axisReverse: false
+				}
+			},
+			'-X': {
+				'back': {
+					next: 'top',
+					axisFrom: 'Y',
+					axisTo: 'Z',
+					axisReverse: true
+				},
+				'bottom': {
+					next: 'back',
+					axisFrom: 'Z',
+					axisTo: 'Y',
+					axisReverse: false
+				},
+				'front': {
+					next: 'bottom',
+					axisFrom: 'Y',
+					axisTo: 'Z',
+					axisReverse: true
+				},
+				'top': {
+					next: 'front',
+					axisFrom: 'Z',
+					axisTo: 'Y',
+					axisReverse: false
+				}
+			},
+			'+Y': {
+				'front': {
+					next: 'right',
+					axisFrom: 'X',
+					axisTo: 'Z',
+					axisReverse: true
+				},
+				'right': {
+					next: 'back',
+					axisFrom: 'Z',
+					axisTo: 'X',
+					axisReverse: false
+				},
+				'back': {
+					next: 'left',
+					axisFrom: 'X',
+					axisTo: 'Z',
+					axisReverse: true
+				},
+				'left': {
+					next: 'front',
+					axisFrom: 'Z',
+					axisTo: 'X',
+					axisReverse: false
+				}
+			},
+			'-Y': {
+				'right': {
+					next: 'front',
+					axisFrom: 'Z',
+					axisTo: 'X',
+					axisReverse: true
+				},
+				'back': {
+					next: 'right',
+					axisFrom: 'X',
+					axisTo: 'Z',
+					axisReverse: false
+				},
+				'left': {
+					next: 'back',
+					axisFrom: 'Z',
+					axisTo: 'X',
+					axisReverse: true
+				},
+				'front': {
+					next: 'left',
+					axisFrom: 'X',
+					axisTo: 'Z',
+					axisReverse: false
+				}
+			},
+			'+Z': {
+				'top': {
+					next: 'right',
+					axisFrom: 'X',
+					axisTo: 'Y',
+					axisReverse: false
+				},
+				'right': {
+					next: 'bottom',
+					axisFrom: 'Y',
+					axisTo: 'X',
+					axisReverse: true
+				},
+				'bottom': {
+					next: 'left',
+					axisFrom: 'X',
+					axisTo: 'Y',
+					axisReverse: false
+				},
+				'left': {
+					next: 'top',
+					axisFrom: 'Y',
+					axisTo: 'X',
+					axisReverse: true
+				}
+			},
+			'-Z': {
+				'right': {
+					next: 'top',
+					axisFrom: 'Y',
+					axisTo: 'X',
+					axisReverse: false
+				},
+				'bottom': {
+					next: 'right',
+					axisFrom: 'X',
+					axisTo: 'Y',
+					axisReverse: true
+				},
+				'left': {
+					next: 'bottom',
+					axisFrom: 'Y',
+					axisTo: 'X',
+					axisReverse: false
+				},
+				'top': {
+					next: 'left',
+					axisFrom: 'X',
+					axisTo: 'Y',
+					axisReverse: true
+				}
+			}
+		}
+	};
 	var buildCubeFragment = (function(){
 	//建立一个rank阶魔方
-		var shareInformation = {};//用于共享信息
 		var buildSurfaces = (function(){
-			var db = {
-			//"数据库"。存储着构建surfaces需要的信息
-			//用于帮助设置小方块的attribute,style.transform等
-			//查询出floor的轴线方向, 部署到各个平面的rotate值
-			//（唉，我去，根本注释不清楚啊。希望我再看这段代码时能看明白。）
-				front: {
-					row: 'X',
-					column: 'Y',
-					color: 'blue',
-					rotate: '',
-					translateAxis: 'Z',
-					translateSign: '+'
-				},
-				left: {
-					row: 'Z',
-					column: 'Y',
-					color: 'orange',
-					rotate: 'rotateY(-90deg)',
-					translateAxis: 'X',
-					translateSign: '-'
-				},
-				top: {
-					row: 'X',
-					column: 'Z',
-					color: 'yellow',
-					rotate: 'rotateX(90deg)',
-					translateAxis: 'Y',
-					translateSign: '-'
-				},
-				back: {
-					row: 'X',
-					column: 'Y',
-					color: 'green',
-					rotate: '',
-					translateAxis: 'Z',
-					translateSign: '-'
-				},
-				right: {
-					row: 'Z',
-					column: 'Y',
-					color: 'red',
-					rotate: 'rotateY(-90deg)',
-					translateAxis: 'X',
-					translateSign: '+'
-				},
-				bottom: {
-					row: 'X',
-					column: 'Z',
-					color: 'whitesmoke',
-					rotate: 'rotateX(90deg)',
-					translateAxis: 'Y',
-					translateSign: '+'
-				}
-			};
-			shareInformation.buildSurfacesDB = db
+			var db = database.buildSurfaces;
 			function buildOne(face){
 				var surface = document.createDocumentFragment();
 				var rank = cubeParameters.rank,
@@ -101,45 +300,7 @@
 			};
 		}());
 		var buildButtons = (function(){
-			var db = {
-			//"数据库"。存储着构建buttons需要的信息（控制旋转信息）
-				front: {
-					columnStart: '+X',//例,查询此条信息可知在front面的最上面的一排按钮的控制行为是：绕x轴正向旋转90度
-					columnEnd: '-X',
-					rowStart: '-Y',
-					rowEnd: '+Y'
-				},
-				left: {
-					columnStart: '+Z',
-					columnEnd: '-Z',
-					rowStart: '-Y',
-					rowEnd: '+Y'
-				},
-				top: {
-					columnStart: '+X',
-					columnEnd: '-X',
-					rowStart: '-Z',
-					rowEnd: '+Z'
-				},
-				back: {
-					columnStart: '-X',
-					columnEnd: '+X',
-					rowStart: '+Y',
-					rowEnd: '-Y'
-				},
-				right: {
-					columnStart: '-Z',
-					columnEnd: '+Z',
-					rowStart: '+Y',
-					rowEnd: '-Y'
-				},
-				bottom: {
-					columnStart: '-X',
-					columnEnd: '+X',
-					rowStart: '+Z',
-					rowEnd: '-Z'
-				}
-			};
+			var db = database.buildButtons;
 			function buildOne(face){
 			//执行此函数将建立指定surface上的buttons
 				var rank = cubeParameters.rank;
@@ -154,8 +315,8 @@
 					controlRotate = '',
 					controlFloors = '',
 					className = '',
-					rotate = shareInformation.buildSurfacesDB[face].rotate,
-					translate = 'translate' + shareInformation.buildSurfacesDB[face].translateAxis + '(' + shareInformation.buildSurfacesDB[face].translateSign + rank / 2 * 111.5 + 'px)';
+					rotate = database.buildSurfaces[face].rotate,
+					translate = 'translate' + database.buildSurfaces[face].translateAxis + '(' + database.buildSurfaces[face].translateSign + rank / 2 * 111.5 + 'px)';
 				function buildSingleButton(){
 				//仅作为buildOne()的内部函数，不需要参数，使用buildOne()中的变量.共有程序段的集中
 					but = document.createElement('div');
@@ -276,10 +437,29 @@
 				return buttons;
 			};
 		}());
+		var buildMasks = function(){
+			var masks = document.createDocumentFragment(),
+				mask = document.createElement('div'),
+				i = 0;
+			mask.setAttribute('mask', '');
+			mask.style.display = 'none';
+			mask.style.backgroundColor = 'white';
+			mask.style.height = mask.style.width = cubeParameters.rank * 110 + 'px';
+			mask.style.borderRadius = '3px';
+			mask.style.position = 'absolute';
+			mask.style.left = mask.style.top = '50%';
+			mask.style.transformOrigin = '0 0';
+			mask.style.transform = 'translate(-50%, -50%)';
+			for(i = 0; i < 4; i++){
+				masks.appendChild( mask.cloneNode(true) );
+			}
+			return masks;
+		};
 		return function(){
 			var cube = document.createDocumentFragment();
 			cube.appendChild( buildSurfaces() );
 			cube.appendChild( buildButtons() );
+			cube.appendChild( buildMasks() );
 			return cube;
 		};
 	}());
@@ -358,7 +538,7 @@
 			buttons[i].addEventListener('click', handler, false);
 		}
 		function selectCells(floors){
-		//floors字符串。例：'X0 X1'
+		//floors字符串。例：'X0 X1'将要被旋转的层
 		//选择将要被旋转的surfaceCells & floorsCells
 			var face = '',
 				axis = floors[0],
@@ -422,7 +602,7 @@
 					}
 					return 3*(1-t)*t*t*1 + t*t*t;	
 				};
-				return function(clockwise, axis, elem){
+				return function(clockwise, axis, elem, callback){
 					var rent = elem.style,
 						rotatePreStr = 'rotate' + axis + '(',
 						rotateOrigin,
@@ -443,13 +623,13 @@
 						tick = setInterval(function(){
 							t += stepPercent;
 							rent.transform = rotatePreStr + ( -yFromXCubicBezier(t)*90 + rotateOrigin ) + styleStrSuf;
-							t > 1 && (rent.transform = rotatePreStr + ( -90 + rotateOrigin ) + styleStrSuf) && clearInterval(tick);
+							t > 1 && (rent.transform = rotatePreStr + ( -90 + rotateOrigin ) + styleStrSuf) && ((callback && callback()) || clearInterval(tick));
 						}, stepTime);
 					} else{
 						tick = setInterval(function(){
 							t += stepPercent;
 							rent.transform = rotatePreStr + ( yFromXCubicBezier(t)*90 + rotateOrigin ) + styleStrSuf;
-							t > 1 && (rent.transform = rotatePreStr + ( 90 + rotateOrigin ) + styleStrSuf) && clearInterval(tick);
+							t > 1 && (rent.transform = rotatePreStr + ( 90 + rotateOrigin ) + styleStrSuf) && ((callback && callback()) || clearInterval(tick));
 						}, stepTime);
 					}
 				};
@@ -458,166 +638,8 @@
 				var rank = cubeParameters.rank,
 					rotateOrigin = (rank - 1) / 2,//这里的rotateOrigin是指旋转的坐标原点
 					pnum = (/[+-]?(?:\d*\.|)\d+(?:[eE][+-]?\d+|)/).source,
-					regexpXZYPnum = new RegExp('([XZY])(' + pnum + ')', 'gi');
-				var db = {
-				//“数据库”
-					'+X': {
-						'top': {
-							next: 'back',
-							axisFrom: 'Z',
-							axisTo: 'Y',
-							axisReverse: true
-						},
-						'back': {
-							next: 'bottom',
-							axisFrom: 'Y',
-							axisTo: 'Z',
-							axisReverse: false
-						},
-						'bottom': {
-							next: 'front',
-							axisFrom: 'Z',
-							axisTo: 'Y',
-							axisReverse: true
-						},
-						'front': {
-							next: 'top',
-							axisFrom: 'Y',
-							axisTo: 'Z',
-							axisReverse: false
-						}
-					},
-					'-X': {
-						'back': {
-							next: 'top',
-							axisFrom: 'Y',
-							axisTo: 'Z',
-							axisReverse: true
-						},
-						'bottom': {
-							next: 'back',
-							axisFrom: 'Z',
-							axisTo: 'Y',
-							axisReverse: false
-						},
-						'front': {
-							next: 'bottom',
-							axisFrom: 'Y',
-							axisTo: 'Z',
-							axisReverse: true
-						},
-						'top': {
-							next: 'front',
-							axisFrom: 'Z',
-							axisTo: 'Y',
-							axisReverse: false
-						}
-					},
-					'+Y': {
-						'front': {
-							next: 'right',
-							axisFrom: 'X',
-							axisTo: 'Z',
-							axisReverse: true
-						},
-						'right': {
-							next: 'back',
-							axisFrom: 'Z',
-							axisTo: 'X',
-							axisReverse: false
-						},
-						'back': {
-							next: 'left',
-							axisFrom: 'X',
-							axisTo: 'Z',
-							axisReverse: true
-						},
-						'left': {
-							next: 'front',
-							axisFrom: 'Z',
-							axisTo: 'X',
-							axisReverse: false
-						}
-					},
-					'-Y': {
-						'right': {
-							next: 'front',
-							axisFrom: 'Z',
-							axisTo: 'X',
-							axisReverse: true
-						},
-						'back': {
-							next: 'right',
-							axisFrom: 'X',
-							axisTo: 'Z',
-							axisReverse: false
-						},
-						'left': {
-							next: 'back',
-							axisFrom: 'Z',
-							axisTo: 'X',
-							axisReverse: true
-						},
-						'front': {
-							next: 'left',
-							axisFrom: 'X',
-							axisTo: 'Z',
-							axisReverse: false
-						}
-					},
-					'+Z': {
-						'top': {
-							next: 'right',
-							axisFrom: 'X',
-							axisTo: 'Y',
-							axisReverse: false
-						},
-						'right': {
-							next: 'bottom',
-							axisFrom: 'Y',
-							axisTo: 'X',
-							axisReverse: true
-						},
-						'bottom': {
-							next: 'left',
-							axisFrom: 'X',
-							axisTo: 'Y',
-							axisReverse: false
-						},
-						'left': {
-							next: 'top',
-							axisFrom: 'Y',
-							axisTo: 'X',
-							axisReverse: true
-						}
-					},
-					'-Z': {
-						'right': {
-							next: 'top',
-							axisFrom: 'Y',
-							axisTo: 'X',
-							axisReverse: false
-						},
-						'bottom': {
-							next: 'right',
-							axisFrom: 'X',
-							axisTo: 'Y',
-							axisReverse: true
-						},
-						'left': {
-							next: 'bottom',
-							axisFrom: 'Y',
-							axisTo: 'X',
-							axisReverse: false
-						},
-						'top': {
-							next: 'left',
-							axisFrom: 'X',
-							axisTo: 'Y',
-							axisReverse: true
-						}
-					}
-				};
+					regexpXZYPnum = new RegExp('([XZY])(' + pnum + ')', 'gi'),
+					db = database.updateAttributeOfCells;
 				return function(rotate, surfaceCells, floorsCells){
 				//rotate,字符串。标明旋转轴和旋转方向。例如：'+X'
 				//surfaceCells floorsCells，数组或类数组。将要更新attribute的surfaceCells & floorsCells
@@ -684,8 +706,62 @@
 					}
 				};
 			}());
-			return function(rotate, surfaceCells, floorsCells){
+			var preventGoof = function(clockwise, axis, floors){
+			//floors字符串。例：'X0 X1'将要被旋转的层
+				var masks = document.querySelectorAll('#cube>[mask]');
+				var rank = cubeParameters.rank,
+					center = rank / 2,
+					cut = [],//需要添加遮罩以防止被‘看穿’的位置，例如cut值为[2, 3]，那么需要在X轴（假设绕X轴旋转）floor2两侧添加mask
+					i = 0,
+					face,
+					needIni = [],
+					db = database.buildSurfaces;//这里借用一下buildSurfaces'数据库'
+				switch(axis){
+					case 'X':
+						face = 'left';
+						break;
+					case 'Y':
+						face = 'top';
+						break;
+					case 'Z':
+						face = 'back';
+						break;
+				}
+				floors = floors.split(' ');
+				cut.push( floors[0][1] );
+				cut.push( +floors[floors.length - 1][1] + 1 + '');
+				//此循环最多只执行两次，逻辑却复杂晦涩。真是失败的设计
+				for(i = 0; i < cut.length; i++){
+					if(+cut[i] === 0 || +cut[i] === rank){
+						continue;
+					}
+					masks[i * 2].style.transform = db[face].rotate + masks[i * 2].style.transform;
+					masks[i * 2].style.transform = 'translate' + axis  + '(' + ((cut[i] - center) * 110 - 1) + 'px)' + masks[i * 2].style.transform;
+					masks[i * 2].style.display = 'block';
+					masks[i * 2 + 1].style.transform = db[face].rotate + masks[i * 2 + 1].style.transform;
+					masks[i * 2 + 1].style.transform = 'translate' + axis  + '(' + ((cut[i] - center) * 110 + 1) + 'px)' + masks[i * 2 + 1].style.transform;
+					masks[i * 2 + 1].style.display = 'block';
+					needIni.push(i * 2);
+					needIni.push(i * 2 + 1);
+					if(i === 0){
+						 +cut[1] === rank
+						 ? rotateOne(clockwise, axis, masks[i * 2 + 1], initialMasks)
+						 : rotateOne(clockwise, axis, masks[i * 2 + 1]);
+					} else{
+						rotateOne(clockwise, axis, masks[i * 2], initialMasks);
+					}
+				}
+				function initialMasks(){
+					var i = 0;
+					for(i = 0; i < needIni.length; i++){
+						masks[ needIni[i] ].style.display = 'none';
+						masks[ needIni[i] ].style.transform = 'translate(-50%, -50%)';
+					}
+				}
+			};
+			return function(rotate, floors, surfaceCells, floorsCells){
 			//rotate,字符串。标明旋转轴和旋转方向。例如：'+X'
+			//floors字符串。例：'X0 X1'将要被旋转的层
 			//surfaceCells, floorsCells.数组或类数组。将要被旋转的surfaceCells & floorsCells
 				var clockwise = rotate[0],
 					axis = rotate[1],
@@ -697,16 +773,17 @@
 					rotateOne(clockwise, axis, floorsCells[i]);
 				}
 				updateAttributeOfCells(rotate, surfaceCells, floorsCells);
+				preventGoof(clockwise, axis, floors);
 			};
 		}());
 		function handler(event){
 			var cells = {};
 			cells = selectCells( this.getAttribute('controlFloors') );
-			rotateCells(this.getAttribute('controlRotate'), cells.surfaceCells, cells.floorsCells);
+			rotateCells(this.getAttribute('controlRotate'), this.getAttribute('controlFloors'), cells.surfaceCells, cells.floorsCells);
 		}
 	};
 	document.addEventListener('DOMContentLoaded', function(){
-		initializeCubeParameter(4);
+		initializeCubeParameter(7);
 		document.getElementById('cube').appendChild( buildCubeFragment() );
 		playerRotateCubeIni();//playerRotateCubeIni()和playerScaleCubeIni()执行顺序不能颠倒，如果有必要，请修改内部实现
 		playerScaleCubeIni();
