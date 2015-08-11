c2613fd6a503f7a7e3bf6a0efc91f02e2325892a
badeggg <zhaoxuxujc@163.com>
2015-08-11 15:25:57 +0800

    开始添加后台服务1

---
+++ js/signInUp.js
@@ -0,0 +1,35 @@
+﻿//2015/8/10 start
+//此文件负责玩家注册/登录
+(function(){
+	var signInLable = document.querySelector('.signInLable'),
+		signUpLable = document.querySelector('.signUpLable'),
+		signIn = document.querySelector('.signIn'),
+		signUp = document.querySelector('.signUp');
+	signInLable.addEventListener('click', function(){
+		signIn.style.display = 'block';
+		signUp.style.display = 'none';
+	}, false);
+	signUpLable.addEventListener('click', function(){
+		signUp.style.display = 'block';
+		signIn.style.display = 'none';
+	}, false);
+}());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
