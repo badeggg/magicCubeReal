c3f004ba80928366ffd48d86a2edba23eaf19be9
badeggg <zhaoxuxujc@163.com>
2015-08-12 20:57:56 +0800

    正在添加后台服务2
    1、使用node的npm下载添加了express模块
    2、基本完成了登录/注册的前端表现代码

---
+++ index.js
@@ -0,0 +1,31 @@
+﻿//2015/8/11 start
+var express = require('express');
+var path = require('path');
+
+var app = express();
+app.use(express.static(__dirname));
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, 'magicCubeReal.html'));
+});
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/signInUp.js
+++ js/signInUp.js
@@ -4,15 +4,62 @@
 	var signInLable = document.querySelector('.signInLable'),
 		signUpLable = document.querySelector('.signUpLable'),
 		signIn = document.querySelector('.signIn'),
-		signUp = document.querySelector('.signUp');
+		signUp = document.querySelector('.signUp'),
+		signBox = document.getElementById('signBox'),
+		close = document.querySelector('.close'),
+		shade = document.querySelector('#shade'),
+		stick = document.querySelector('.stick');
+	signBox.addEventListener('mouseenter', handler1, false);
+	signBox.addEventListener('mouseleave', handler2, false);
 	signInLable.addEventListener('click', function(){
 		signIn.style.display = 'block';
 		signUp.style.display = 'none';
+		close.style.display = 'block';
+		shade.style.display = 'block';
+		this.style.borderBottom = 'none';
+		signUpLable.style.borderBottom = '2px solid white';
+		try{
+			signBox.removeEventListener('mouseenter', handler1, false);
+			signBox.removeEventListener('mouseleave', handler2, false);
+		}catch(err){
+			console.log(err);
+		}
 	}, false);
 	signUpLable.addEventListener('click', function(){
 		signUp.style.display = 'block';
 		signIn.style.display = 'none';
+		close.style.display = 'block';
+		shade.style.display = 'block';
+		this.style.borderBottom = 'none';
+		signInLable.style.borderBottom = '2px solid white';
+		try{
+			signBox.removeEventListener('mouseenter', handler1, false);
+			signBox.removeEventListener('mouseleave', handler2, false);
+		}catch(err){
+			console.log(err);
+		}
 	}, false);
+	close.addEventListener('click', function(){
+		signUp.style.display = 'none';
+		signIn.style.display = 'none';
+		close.style.display = 'none';
+		shade.style.display = 'none';
+		signUpLable.style.width = '0px';
+		signInLable.style.borderBottom = 'none';
+		signUpLable.style.borderBottom = 'none';
+		signBox.addEventListener('mouseenter', handler1, false);
+		signBox.addEventListener('mouseleave', handler2, false);
+	}, false);
+	function handler1(){
+	//handler to show signUpLable
+		signUpLable.style.width = '150px';
+		stick.style.display = 'inline-block';
+	};
+	function handler2(){
+	//handler to hide signUpLable
+		signUpLable.style.width = '0px';
+		stick.style.display = 'none';
+	};
 }());
 
 
